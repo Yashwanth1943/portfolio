c66a9603b48e1650050d20f4a0e17fd1169dbe7f
Yashwanth1943 <yashwanthkosuri@gmail.com>
2026-07-16 21:24:54 +0530

    fixed nodemailer

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,8 +1,6 @@
-import dns from "dns";
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
-dns.setDefaultResultOrder("ipv4first");
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
@@ -15,7 +13,7 @@ const transporter = nodemailer.createTransport({
 
 transporter.verify((error) => {
     if (error) {
-        console.error("❌ Mail configuration error:", error);
+        console.error(error);
     } else {
         console.log("✅ Mail server is ready");
     }
