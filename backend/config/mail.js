672352e1e0047ffcaa7199d8b46fff09a608c989
Yashwanth1943 <yashwanthkosuri@gmail.com>
2026-07-16 21:19:20 +0530

    Fix nodemailer issue

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -6,17 +6,13 @@ dns.setDefaultResultOrder("ipv4first");
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    family: 4,
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
 });
 
-// Verify SMTP connection when server starts
 transporter.verify((error) => {
     if (error) {
         console.error("❌ Mail configuration error:", error);
