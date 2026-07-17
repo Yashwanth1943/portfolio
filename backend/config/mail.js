e5d15dd62fb13480fc9d045a311695b3bf3e2b50
Yashwanth1943 <yashwanthkosuri@gmail.com>
2026-07-17 10:30:49 +0530

    fixed brevo

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -8,7 +8,7 @@ console.log("BREVO_PASS:", process.env.BREVO_PASS ? "Loaded" : "Missing");
 
 const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-    port: 587,
+    port: 2525,
     secure: false,
     auth: {
         user: process.env.BREVO_USER,
